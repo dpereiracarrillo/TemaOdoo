8ad22863068c09aaade4debeb078a01c00d0618e
dpereiracarrillo <dpereiracarrillo@danielcastelao.org>
2016-02-24 10:49:40 +0100

    commit inicial

---
+++ theme_miTema/__openerp__.py
@@ -0,0 +1,12 @@
+{
+'name':'MiTema',
+'description':'el buen tema',
+'version':'0.5',
+'author':'Er Danih',
+'category':'Theme/Hidden',
+'data':'['views/templates.xml',],
+'sequence':100,
+'images':'['static/descrition/cover.png',],
+'depends':['website'],
+'application':False,
+}
